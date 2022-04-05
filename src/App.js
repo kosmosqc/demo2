fa8f5592d20b683aad75d05c3cfbf9e96cb8bef1
Fairl <Fairl@MAIN>
2022-04-04 21:39:49 -0400

    gg

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  document.title = "Alain Gionet"
   return (
     <Router>
      <Routes>
